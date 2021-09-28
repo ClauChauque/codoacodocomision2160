151b53d65336e30419f31771f1b841f7f035cde9
claudia <claudiachauque9@gmail.com>
2021-09-27 21:05:27 -0300

    bootstrap: footer, formulario, navbar, clases

---
+++ javascript/js/app.js
@@ -0,0 +1,39 @@
+alert('soy una ventana emergente!')
+/*Variables*/
+/*palabra reservada nombre de la variable*/
+var nombre_variable;
+/*palabra reservada nombre de la variable = asignamos al valor */
+let otra_variable = "hola"; /*Tipo STRING */
+const PI = 3.1416; /*Tipo enteros y float*/
+let num2=18;
+let hayClases=true; /*Tipo boolean*/ 
+let terminoLaClase=false; 
+/*Tipos, undefined, numero, String Boolean, Objeto, arreglo, chart, función */
+console.long(PI*num2);
+console.long();
+
+if(){
+    console.long('Nos conectamos a la videollamada')
+} else {
+    console.long('Salimos a celebrar el día del estudiante')
+}
+if(hayClases == false || ){
+    console.long('Salimos a celebrar el día del estudiante')
+}
+
+let mensaje = prompt("Introduce un tu mensaje: ");
+elemento.innerHTML+="<h3>" + mensaje+ "<h3>";
+elemento.innerHTML+= <div class=\"alert alert-success\" role=\"alert\">"+mensaje+"<div/>;
+
+let menu = prompt("Selecciona el pedido: )
+
+/*switch (menu) {
+    case '1':
+        elemnto.innerHTML="<div class\"alert alert-success\" role=\"alert\">Disfruta de tus milanesas con fritas        
+        break;
+
+    default:
+        elemento.innerHTML="<div class\"alert alert-success\" role=\"alert\">Debes elegir algo del menú
+        break;
+} */
+
